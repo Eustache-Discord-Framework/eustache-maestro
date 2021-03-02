5be83fa1169d7634a21666681925f7d14320e029
gmvqbu <gmvqbu@users.noreply.github.com>
2021-03-02 13:59:38 +0100

    feat: fichier principal

---
+++ src/robot.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require('module-alias/register');
+
+const { EustacheClient } = require('eustache-discord-framework');
+const { setUsername } = require('./util.js');
+const config = require('./config.json');
+
+const client = new EustacheClient({
+    prefix: config.bot.prefix,
+    ownerID: config.bot.owner
+});
+
+client.registry
+    .registerDefaults()
+    .registerTypes([
+        require('@types/yt-query')
+    ])
+    .registerCommands([
+        // Player
+        require('@commands/player/play'),
+        require('@commands/player/stop'),
+        require('@commands/player/pause'),
+        require('@commands/player/resume'),
+        require('@commands/player/next'),
+        // Queue
+        require('@commands/queue/queue'),
+        require('@commands/queue/empty'),
+        require('@commands/queue/shuffle')
+    ]);
+
+client.on('ready', () => {
+    // This part is not in the client file cuz it requires the config
+    const bot = client.user;
+    console.log(`Logged in as ${bot.tag} !`);
+    if (bot.username != Config.username) setUsername(bot, Config.username);
+    bot.setActivity('')
+});
+
+client.login(config.bot.token);
